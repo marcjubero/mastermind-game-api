2189cd3913a95ea8fbca4c30d21a4475920a2352
marcjubero <marcjubero@gmail.com>
2019-06-15 20:19:15 +0200

    Game Service - guess evaluation algorithm

--- mastermindgameapi/services/game_service.py
+++ mastermindgameapi/services/game_service.py
@@ -4,4 +4,14 @@ class GameService:
         pass
 
     def eval_guess(self, secret_code, guess):
-        return None
+        secret_cpy = secret_code.copy()
+
+        blacks = [1 for secret, guess_item in zip(secret_cpy, guess) if secret == guess_item]
+
+        whites = []
+        for g in guess:
+            if g in secret_cpy:
+                secret_cpy[secret_cpy.index(g)] = None
+                whites.append(0)
+
+        return whites if len(blacks) == 0 else (blacks + whites[:-len(blacks)])
