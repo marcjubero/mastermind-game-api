64eec402e60e14798072465b7f632f35a3be7003
mjubero <marc.jubero@genomcore.com>
2019-06-18 09:24:59 +0200

    More tests

--- mastermindgameapi/controllers/game_controller.py
+++ mastermindgameapi/controllers/game_controller.py
@@ -5,6 +5,7 @@ from flask_restful import Api, Resource
 from werkzeug.exceptions import HTTPException
 
 from mastermindgameapi.config.game_config import MAX_TURNS
+from mastermindgameapi.data.exceptions import DataNotFoundException
 from mastermindgameapi.data.repository.game_repository import GameRepository
 from mastermindgameapi.data.repository.guess_repository import GuessRepository
 from mastermindgameapi.models.exceptions import InvalidGuessLength
@@ -32,28 +33,33 @@ class GamesController(Resource):
         except InvalidGuessLength as e:
             return make_response(e.args[0], 400)
         except KeyError as e:
-            return make_response('Invalid peg color:{0} '.format(e.args[0]), 400)
+            return make_response('{0} is not a valid color'.format(e.args[0]), 400)
         except Exception as e:
             return make_response(e.args[0], 500)
 
     def get(self):
         serialized = [self._game_repo.dump(g) for g in self._game_repo.all()]
-        response = make_response(json.dumps(serialized), 200)
+        response_data = (json.dumps(serialized), 200) if len(serialized) > 0 else ('', 204)
+        response = make_response(response_data[0], response_data[1])
         response.headers['Content-Type'] = 'application/json'
         return response
 
 
 class GameController(Resource):
-
     def __init__(self) -> None:
         super().__init__()
         self._game_repo = GameRepository()
 
     def get(self, game_id):
-        game = self._game_repo.first_or_raise(**{'id': str(game_id)})
-        response = make_response(json.dumps(self._game_repo.dump(game)))
-        response.headers['Content-Type'] = 'application/json'
-        return response
+        try:
+            game = self._game_repo.first_or_raise(**{'id': str(game_id)})
+            response = make_response(json.dumps(self._game_repo.dump(game)))
+            response.headers['Content-Type'] = 'application/json'
+            return response
+        except DataNotFoundException as _:
+            return make_response('Game with id \'{0}\' not found'.format(game_id), 404)
+        except Exception as e:
+            return make_response(e.args[0], 500)
 
 
 class GameGuessController(Resource):
@@ -76,9 +82,14 @@ class GameGuessController(Resource):
             game_model = Game(secret=Guess(game_data.secret.split(';')))
             return make_response(json.dumps({'result': game_model.eval_guess(guess).values}))
 
-        except HTTPException as e:
-            return make_response()
-            print(e)
+        except DataNotFoundException as _:
+            return make_response('Game with id \'{0}\' not found'.format(game_id), 404)
+        except InvalidGuessLength as e:
+            return make_response(e.args[0], 400)
+        except KeyError as e:
+            return make_response('{0} is not a valid color'.format(e.args[0]), 400)
+        except Exception as e:
+            return make_response(e.args[0], 500)
 
 
 api.add_resource(GamesController, '/games')
